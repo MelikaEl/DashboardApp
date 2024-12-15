536b0b9b62227f8ac2cf5648fac8fadc6d67490a
MelikaEl <melika.melika8@gmail.com>
2024-12-15 19:56:37 +0330

    change theme of sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,39 +21,39 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <Command className='bg-secondary rounded-none'>
+    <Command className='bg-pink-200 dark:bg-slate-800 rounded-none'>
       <CommandInput placeholder='Type a command or search...' />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading='Suggestions'>
           <CommandItem>
-            <LayoutDashboard className='mr-2 h-4 w-4' />
-            <Link href='/'>Dashboard</Link>
+            <LayoutDashboard className='mr-2 h-4 w-4 ' />
+            <Link href='/' className='text-pink-500 dark:text-pink-300'>Dashboard</Link>
           </CommandItem>
           <CommandItem>
             <Newspaper className='mr-2 h-4 w-4' />
-            <Link href='/posts'>Posts</Link>
+            <Link href='/posts' className='text-pink-500 dark:text-pink-300'>Posts</Link>
           </CommandItem>
           <CommandItem>
             <Folders className='mr-2 h-4 w-4' />
-            <Link href='#'>Categories</Link>
+            <Link href='#' className='text-pink-500 dark:text-pink-300'>Categories</Link>
           </CommandItem>
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading='Settings'>
           <CommandItem>
             <User className='mr-2 h-4 w-4' />
-            <span>Profile</span>
+            <span className='text-pink-500 dark:text-pink-300'>Profile</span>
             <CommandShortcut>⌘P</CommandShortcut>
           </CommandItem>
           <CommandItem>
             <CreditCard className='mr-2 h-4 w-4' />
-            <span>Billing</span>
+            <span className='text-pink-500 dark:text-pink-300'>Billing</span>
             <CommandShortcut>⌘B</CommandShortcut>
           </CommandItem>
           <CommandItem>
             <Settings className='mr-2 h-4 w-4' />
-            <span>Settings</span>
+            <span className='text-pink-500 dark:text-pink-300'>Settings</span>
             <CommandShortcut>⌘S</CommandShortcut>
           </CommandItem>
         </CommandGroup>
