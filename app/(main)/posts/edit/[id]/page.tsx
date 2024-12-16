071244e2aa20f409582e15a3ccdd13b41032b9ea
MelikaEl <melika.melika8@gmail.com>
2024-12-16 13:20:34 +0330

    change the theme of edit page

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -64,7 +64,7 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
   return (
     <>
       <BackButton text='Back To Posts' link='/posts' />
-      <h3 className='text-2xl mb-4'>Edit Post</h3>
+      <h3 className='text-2xl mb-4 dark:text-pink-300'>Edit Post</h3>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-8'>
           <FormField
@@ -77,7 +77,7 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                    className='bg-pink-200 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
                     placeholder='Enter Title'
                     {...field}
                   />
@@ -97,7 +97,7 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
                 </FormLabel>
                 <FormControl>
                   <Textarea
-                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                    className='bg-pink-200 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
                     placeholder='Enter Body'
                     {...field}
                   />
@@ -117,7 +117,7 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                    className='bg-pink-200 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
                     placeholder='Enter Author'
                     {...field}
                   />
@@ -137,7 +137,7 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                    className='bg-pink-200 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
                     placeholder='Enter Date'
                     {...field}
                   />
@@ -147,7 +147,7 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
             )}
           />
 
-          <Button className='w-full dark:bg-slate-800 dark:text-white'>
+          <Button className='w-full dark:bg-slate-800 dark:text-pink-300'>
             Update Post
           </Button>
         </form>
