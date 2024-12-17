a6892cae7b428dc2d4887a490189ca444d168180
MelikaEl <melika.melika8@gmail.com>
2024-12-17 21:32:44 +0330

    add firebase authentication to the login form

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import BackButton from '@/components/BackButton';
-import * as z from 'zod';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import BackButton from "@/components/BackButton";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -11,73 +11,96 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
+} from "@/components/ui/form";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
+import { auth } from "@/app/firebase/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useToast } from "@/components/ui/use-toast";
 
 const formSchema = z.object({
   email: z
     .string()
     .min(1, {
-      message: 'Email is required',
+      message: "Email is required",
     })
     .email({
-      message: 'Please enter a valid email',
+      message: "Please enter a valid email",
     }),
   password: z.string().min(1, {
-    message: 'Password is required',
+    message: "Password is required",
   }),
 });
 
 const LoginForm = () => {
   const router = useRouter();
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
   });
 
-  const handleSubmit = (data: z.infer<typeof formSchema>) => {
-    router.push('/');
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      toast({
+        title: "Success",
+        description: "Successfully logged in!",
+      });
+      router.push("/");
+    } catch (error: any) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: error.message,
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <Card className='bg-pink-200 dark:bg-slate-950 '>
+    <Card className="bg-pink-200 dark:bg-slate-950 ">
       <CardHeader>
-        <CardTitle className='dark:text-pink-300' >Login</CardTitle>
+        <CardTitle className="dark:text-pink-300">Login</CardTitle>
         <CardDescription>
           Log into your account with your credentials
         </CardDescription>
       </CardHeader>
-      <CardContent className='space-y-2'>
+      <CardContent className="space-y-2">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(handleSubmit)}
-            className='space-y-6'
+            className="space-y-6"
           >
             <FormField
               control={form.control}
-              name='email'
+              name="email"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-pink-300'>
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-pink-300">
                     Email
                   </FormLabel>
                   <FormControl>
                     <Input
-                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0'
-                      placeholder='Enter Email'
+                      className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0"
+                      placeholder="Enter Email"
                       {...field}
                     />
                   </FormControl>
@@ -88,17 +111,17 @@ const LoginForm = () => {
 
             <FormField
               control={form.control}
-              name='password'
+              name="password"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-pink-300'>
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-pink-300">
                     Password
                   </FormLabel>
                   <FormControl>
                     <Input
-                      type='password'
-                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0'
-                      placeholder='Enter Password'
+                      type="password"
+                      className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0"
+                      placeholder="Enter Password"
                       {...field}
                     />
                   </FormControl>
@@ -107,7 +130,9 @@ const LoginForm = () => {
               )}
             />
 
-            <Button className='w-full dark:bg-pink-300'>Sign In</Button>
+            <Button disabled={isLoading} className="w-full dark:bg-pink-300">
+              {isLoading ? "Signing in..." : "Sign In"}
+            </Button>
           </form>
         </Form>
       </CardContent>
