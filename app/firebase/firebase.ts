4c434a87d32d181da45cca9b5af057285fbbb1e4
MelikaEl <melika.melika8@gmail.com>
2024-12-21 14:40:25 +0330

    change the export db format

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -13,5 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const db = getFirestore(app);
-export { db };
+export const db = getFirestore(app);
+// export { db };
