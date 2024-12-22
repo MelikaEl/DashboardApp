1fb629a227fa8b5cbedd29f4ad46622556d21abc
MelikaEl <melika.melika8@gmail.com>
2024-12-22 11:36:32 +0330

    refactor firestore operations to use real-time updates for posts with onSnapshot

--- app/firebase/firestoreoperations.ts
+++ app/firebase/firestoreoperations.ts
@@ -1,20 +1,15 @@
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
-import { db } from '@/app/firebase/firebase';
-import { Post } from '@/types/posts';
+import { collection, onSnapshot, doc } from "firebase/firestore";
+import { db } from "@/app/firebase/firebase";
+import { Post } from "@/types/posts";
 
-export async function getPosts(): Promise<Post[]> {
-  const posts: Post[] = [];
-  try {
-   const postsRef = collection(db, 'posts');
-   const q = query(postsRef, orderBy('date', 'desc'));
-   const querySnapshot = await getDocs(q);
-   
-   querySnapshot.forEach((doc) => {
-     posts.push({ id: doc.id, ...doc.data() } as Post);
-   });
-   return posts;
- } catch (error) {
-   console.error('Error getting posts:', error);
-   return [];
- }
+export function subscribeToPosts(callback: (posts: Post[]) => void) {
+  const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
+    const posts: Post[] = [];
+    snapshot.forEach((doc) => {
+      posts.push({ id: doc.id, ...doc.data() } as Post);
+    });
+    callback(posts);
+  });
+
+  return unsubscribe;
 }
