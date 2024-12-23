82dcd613a0b2205fcf83ee1fe7886e1e95fc3297
MelikaEl <melika.melika8@gmail.com>
2024-12-23 12:44:49 +0330

    add CreatePostButton and update layout in PostsPage component

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,11 +1,17 @@
-import PostsTable from '@/components/posts/PostsTable';
-import BackButton from '@/components/BackButton';
-import PostsPagination from '@/components/posts/PostsPagination';
+import PostsTable from "@/components/posts/PostsTable";
+import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
+import CreatePostButton from "@/components/CreatePostButton";
 
 const PostsPage = () => {
   return (
     <>
-      <BackButton text='Go Back' link='/' />
+      <BackButton text="Go Back" link="/" />
+
+      <div className="flex justify-between items-center ">
+        <h3 className="text-2xl mb-4 dark:text-pink-300">Posts</h3>
+        <CreatePostButton />
+      </div>
       <PostsTable />
       <PostsPagination />
     </>
