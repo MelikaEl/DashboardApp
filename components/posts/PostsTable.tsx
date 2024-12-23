9ff6be9e131093474e6601eafa04b23521d25cf7
MelikaEl <melika.melika8@gmail.com>
2024-12-23 10:19:33 +0330

    add delete functionality to PostsTable component with toast notifications

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -14,14 +14,18 @@ import Link from "next/link";
 import { Post } from "@/types/posts";
 import { useEffect, useState } from "react";
 // import { getPosts } from "@/app/firebase/firestoreoperations";
-import {subscribeToPosts} from "@/app/firebase/firestoreoperations"
+import {
+  subscribeToPosts,
+  deletePost,
+} from "@/app/firebase/firestoreoperations";
+import { Trash2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
 
 interface PostsTableProps {
   // limit?: number;
   title?: string;
   post?: string;
   author?: string;
-
 }
 
 // const PostsTable = ({ limit, title }: PostsTableProps) => {
@@ -36,6 +40,7 @@ const PostsTable = ({ post, title }: PostsTableProps) => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const { toast } = useToast();
 
   useEffect(() => {
     const unsubscribe = subscribeToPosts((fetchedPosts) => {
@@ -54,6 +59,26 @@ const PostsTable = ({ post, title }: PostsTableProps) => {
   // if (loading) return <div>Loading posts...</div>;
   // if (error) return <div>Error: {error}</div>;
 
+  const handleDelete = async (postId: string) => {
+    try {
+      const success = await deletePost(postId);
+      if (success) {
+        toast({
+          title: "Success",
+          description: "Post deleted successfully",
+        });
+      } else {
+        throw new Error("Failed to delete post");
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Failed to delete post",
+      });
+    }
+  };
+
   return (
     <div className="mt-10">
       {/* <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3> */}
@@ -67,6 +92,7 @@ const PostsTable = ({ post, title }: PostsTableProps) => {
               Date
             </TableHead> */}
             <TableHead>View</TableHead>
+            <TableHead>Action</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -85,6 +111,14 @@ const PostsTable = ({ post, title }: PostsTableProps) => {
                     Edit
                   </button>
                 </Link>
+              </TableCell>
+              <TableCell>
+                <button
+                  onClick={() => handleDelete(post.id)}
+                  className="bg-pink-300 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded text-xs"
+                >
+                  <Trash2 color="#fffafa" size={18} />
+                </button>
               </TableCell>
             </TableRow>
           ))}
