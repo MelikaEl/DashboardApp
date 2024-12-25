f5f33b13bccf42d4673ceae6c98a31bb4a5cc69e
MelikaEl <melika.melika8@gmail.com>
2024-12-25 10:27:16 +0330

    eliminate some items from sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,53 +8,35 @@ import {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from '@/components/ui/command';
-import {
-  LayoutDashboard,
-  Newspaper,
-  Folders,
-  CreditCard,
-  Settings,
-  User,
-} from 'lucide-react';
-import Link from 'next/link';
+} from "@/components/ui/command";
+import { LayoutDashboard, Newspaper, User } from "lucide-react";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <Command className='bg-pink-200 dark:bg-slate-800 rounded-none h-full'>
-      <CommandInput placeholder='Type a command or search...' />
-      <CommandList  className="h-[calc(100vh-4rem)]">
+    <Command className="bg-pink-200 dark:bg-slate-800 rounded-none h-full">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList className="h-[calc(100vh-4rem)]">
         <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading='Suggestions'>
+        <CommandGroup heading="Suggestions">
           <CommandItem>
-            <LayoutDashboard className='mr-2 h-4 w-4 ' />
-            <Link href='/' className='text-pink-500 dark:text-pink-300'>Dashboard</Link>
+            <LayoutDashboard className="mr-2 h-4 w-4 " />
+            <Link href="/" className="text-pink-500 dark:text-pink-300">
+              Dashboard
+            </Link>
           </CommandItem>
           <CommandItem>
-            <Newspaper className='mr-2 h-4 w-4' />
-            <Link href='/posts' className='text-pink-500 dark:text-pink-300'>Posts</Link>
-          </CommandItem>
-          <CommandItem>
-            <Folders className='mr-2 h-4 w-4' />
-            <Link href='#' className='text-pink-500 dark:text-pink-300'>Categories</Link>
+            <Newspaper className="mr-2 h-4 w-4" />
+            <Link href="/posts" className="text-pink-500 dark:text-pink-300">
+              Posts
+            </Link>
           </CommandItem>
         </CommandGroup>
         <CommandSeparator />
-        <CommandGroup heading='Settings'>
+        <CommandGroup heading="Settings">
           <CommandItem>
-            <User className='mr-2 h-4 w-4' />
-            <span className='text-pink-500 dark:text-pink-300'>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard className='mr-2 h-4 w-4' />
-            <span className='text-pink-500 dark:text-pink-300'>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings className='mr-2 h-4 w-4' />
-            <span className='text-pink-500 dark:text-pink-300'>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
+            <User className="mr-2 h-4 w-4" />
+            <span className="text-pink-500 dark:text-pink-300">Profile</span>
           </CommandItem>
         </CommandGroup>
       </CommandList>
