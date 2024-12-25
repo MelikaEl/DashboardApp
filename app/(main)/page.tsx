0516c6d78495862ec214776d6598ea6c535a30eb
MelikaEl <melika.melika8@gmail.com>
2024-12-25 10:18:06 +0330

    enhance PostsTable with pagination props

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,36 +1,45 @@
-import DashboardCard from '@/components/dashboard/DashboardCard';
-import PostsTable from '@/components/posts/PostsTable';
-import AnalyticsChart from '@/components/dashboard/AnalyticsChart';
-import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
         <DashboardCard
-          title='Posts'
+          title="Posts"
           count={100}
-          icon={<Newspaper className='text-pink-500 dark:text-pink-300'  size={72} />}
+          icon={
+            <Newspaper className="text-pink-500 dark:text-pink-300" size={72} />
+          }
         />
         <DashboardCard
-          title='Categories'
+          title="Categories"
           count={12}
-          icon={<Folder className='text-pink-500   dark:text-pink-300' size={72} />}
+          icon={
+            <Folder className="text-pink-500   dark:text-pink-300" size={72} />
+          }
         />
         <DashboardCard
-          title='Users'
+          title="Users"
           count={750}
-          icon={<User className='text-pink-500 dark:text-pink-300'  size={72} />}
+          icon={<User className="text-pink-500 dark:text-pink-300" size={72} />}
         />
         <DashboardCard
-          title='Comments'
+          title="Comments"
           count={1200}
-          icon={<MessageCircle className='text-pink-500 dark:text-pink-300'  size={72} />}
+          icon={
+            <MessageCircle
+              className="text-pink-500 dark:text-pink-300"
+              size={72}
+            />
+          }
         />
       </div>
       <AnalyticsChart />
       {/* <PostsTable title='Latest Posts' limit={5} /> */}
-      <PostsTable title='Latest Posts'  />
+      <PostsTable title="Latest Posts" currentPage={1} postsPerPage={5} />
     </>
   );
 }
