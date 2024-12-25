0973bb430bae80cd5c3f94db0c7af11f78748350
MelikaEl <melika.melika8@gmail.com>
2024-12-25 10:13:04 +0330

    add pagination to PostsPage component with state management

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import PostsTable from "@/components/posts/PostsTable";
 import BackButton from "@/components/BackButton";
 import PostsPagination from "@/components/posts/PostsPagination";
 import CreatePostButton from "@/components/CreatePostButton";
+import { useState } from "react";
 
 const PostsPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 5;
   return (
     <>
       <BackButton text="Go Back" link="/" />
 
       <div className="flex justify-between items-center ">
         <h3 className="text-2xl mb-4 dark:text-pink-300">Posts</h3>
-        <CreatePostButton link="/posts/create"/>
+        <CreatePostButton link="/posts/create" />
       </div>
-      <PostsTable />
-      <PostsPagination />
+      <PostsTable currentPage={currentPage} postsPerPage={postsPerPage} />
+      <PostsPagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        postsPerPage={postsPerPage}
+      />
     </>
   );
 };
