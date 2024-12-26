8ac56647123eaf4df395ba676d6b496a89678e11
MelikaEl <melika.melika8@gmail.com>
2024-12-26 13:41:22 +0330

    replace DashboardCard components with DashboardStats in Home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,4 @@
-import DashboardCard from "@/components/dashboard/DashboardCard";
+import DashboardStats from "@/components/dashboard/DashboardStats";
 import PostsTable from "@/components/posts/PostsTable";
 import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
@@ -6,39 +6,8 @@ import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-        <DashboardCard
-          title="Posts"
-          count={100}
-          icon={
-            <Newspaper className="text-pink-500 dark:text-pink-300" size={72} />
-          }
-        />
-        <DashboardCard
-          title="Categories"
-          count={12}
-          icon={
-            <Folder className="text-pink-500   dark:text-pink-300" size={72} />
-          }
-        />
-        <DashboardCard
-          title="Users"
-          count={750}
-          icon={<User className="text-pink-500 dark:text-pink-300" size={72} />}
-        />
-        <DashboardCard
-          title="Comments"
-          count={1200}
-          icon={
-            <MessageCircle
-              className="text-pink-500 dark:text-pink-300"
-              size={72}
-            />
-          }
-        />
-      </div>
+      <DashboardStats />
       <AnalyticsChart />
-      {/* <PostsTable title='Latest Posts' limit={5} /> */}
       <PostsTable title="Latest Posts" currentPage={1} postsPerPage={5} />
     </>
   );
